5bad0e985b001604ba677fd81edb97998d58c5cc
Nicolas Minoli <nminoli@primary.com.ar>
2020-12-18 11:24:55 -0300

    Adding initial project layout

---
+++ src/main/java/com/exercise/lottolandRPS/LottolandRpsApplication.java
@@ -0,0 +1,12 @@
+package com.exercise.lottolandRPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottolandRpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LottolandRpsApplication.class, args);
+    }
+}
